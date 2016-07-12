1301345078c5fcab537a45a24d516f3b158d1012
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:53 +0200

    Update test to version 4.0

--- tests/test_stock_external_reception.py
+++ tests/test_stock_external_reception.py
@@ -4,23 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_external_reception')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_external_reception')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_external_reception'
 
 
 def suite():
@@ -29,5 +20,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_stock_external_reception.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
