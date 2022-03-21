55d15513362fef54a3ac9fd77d207b0e10f2cd35
Raimon Esteve <raimonesteve@gmail.com>
2022-03-21 19:32:32 +0100

    Check rule only if _check_access is set and enforce companies rule issue4080

--- tests/test_stock_external_reception.py
+++ tests/test_stock_external_reception.py
@@ -7,9 +7,10 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class TestCase(ModuleTestCase):
+class TestCase(CompanyTestMixin, ModuleTestCase):
     'Test module'
     module = 'stock_external_reception'
 
